791f95dcb099d6c6039411949481f784c3cb6750
BisiDavi <oludavidconnect@gmail.com>
2021-03-17 16:51:12 +0100

    config

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,12 +1,20 @@
+const { parse } = require("pg-connection-string");
+
 module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
   return {
     defaultConnection: "default",
     connections: {
       default: {
         connector: "bookshelf",
         settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
         },
         options: {
           useNullAsDefault: true,
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
-};
+if (process.env.NODE_ENV === "development") {
+    module.exports = {
+        images: {
+            domains: ["localhost"],
+        },
+    };
+} else if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        images: {
+            domains: ["res.cloudinary.com"],
+        },
+    };
+}
