da54f19709a88343efbb68d857986cc4ad71d8ca
BisiDavi <oludavidconnect@gmail.com>
2021-03-18 22:11:16 +0100

    pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aed94ab05249eafd84c3";
+/******/ 	var hotCurrentHash = "308526c77123f06bc0d1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
