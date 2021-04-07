43fed922dec99d4ec08e7c7be46cd8760d4625e2
BisiDavi <oludavidconnect@gmail.com>
2021-04-07 18:31:35 +0100

    api code config

--- pages/api/instagram/[token].ts
+++ pages/api/instagram/[token].ts
@@ -23,11 +23,10 @@ const InstagramtokenHandler = async (req, res) => {
     const url = `${process.env.NEXT_PUBLIC_TOKEN_BASE_URL}/oauth/access_token/client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID}/client_secret=${process.env.NEXT_PUBLIC_CLIENT_SECRET}/grant_type=authorization_code/redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}/code=${token}`;
 
     await runMiddleware(req, res, cors);
-    res.setHeader("Access-Control-Allow-Origin", "*");
     axios
         .post(url)
         .then(({ data }) => res.status(200).json({ data }))
-        .catch((error) => res.status(400).json({ error }));
+        .catch((error) => console.log("error", error));
 };
 
 export default InstagramtokenHandler;
