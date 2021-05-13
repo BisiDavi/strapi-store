e768d44a5aacf465478a2617b16a8ee9e4619a79
BisiDavi <oludavidconnect@gmail.com>
2021-05-13 01:53:35 +0100

    api fix

--- pages/api/cart-notification.ts
+++ pages/api/cart-notification.ts
@@ -1,7 +1,20 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 export const CartNotification = (req, res) => {
-    const transporter = nodemailer.createTransport({});
+    const p = process.env.MAIL_PASSWORD;
+    console.log('password', p);
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'oludavidconnect@gmail.com',
+            pass: process.env.MAIL_PASSWORD,
+        },
+        secure: true,
+    });
 
     console.log(req.body);
-}
+    // res.status(200).json('Hello, user');
+};
+
+export default CartNotification;
