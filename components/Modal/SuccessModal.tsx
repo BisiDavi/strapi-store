ad79dd83f1a51bd6f85e0dedce1e01c70b69de3b
BisiDavi <oludavidconnect@gmail.com>
2021-05-13 18:10:28 +0100

    modal fix

--- components/Modal/SuccessModal.tsx
+++ components/Modal/SuccessModal.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { CSSTransition } from 'react-transition-group';
 import { BsCheckCircle } from 'react-icons/bs';
 import { PageModal } from '.';
 
@@ -7,16 +6,35 @@ const SuccessModal = ({ modal, onHide, content }) => {
     console.log('successModal loaded');
     return (
         <PageModal show={modal} onHide={onHide}>
-            <div className='modal'>
-                <div className='icon'>
-                    <CSSTransition in={modal} timeout={300}>
-                        <BsCheckCircle />
-                    </CSSTransition>
+            <div className='modal-content'>
+                <div className='icon my-3'>
+                    <BsCheckCircle />
                 </div>
                 <h1>
                     Hello ${content.payer.name.given_name}, Payment successfully
                     made, thanks for shopping with us.
                 </h1>
+                <p className='my-3'>
+                    Expect your ordered item delivered to you within 4 - 10
+                    working days
+                </p>
+                <style jsx>
+                    {`
+                        .modal-content {
+                            display: flex;
+                            flex-direction: column;
+                            margin-top: -20px;
+                        }
+                        .icon {
+                            margin: auto;
+                        }
+                        .modal-content h1,
+                        p {
+                            text-align: center;
+                            color: black;
+                        }
+                    `}
+                </style>
             </div>
         </PageModal>
     );
