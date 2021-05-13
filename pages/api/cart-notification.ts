cc7a5937673cc698ebb607747f643194089994fa
BisiDavi <oludavidconnect@gmail.com>
2021-05-13 03:06:13 +0100

    main

--- pages/api/cart-notification.ts
+++ pages/api/cart-notification.ts
@@ -13,6 +13,19 @@ export const CartNotification = (req, res) => {
         secure: true,
     });
 
+    const mailData = {
+        from: "Jenjen's Luxury Wigs",
+        to: 'oludavidconnect@gmail.com',
+        subject: `Add to Cart Notification, ${req.body.userName} just added to cart!`,
+        html: `<div>${req.body.userName} added ${req.body.cart} to cart, the cost is ${req.body.amount}</div>`,
+    };
+
+    transporter.sendMail(mailData, function(err, info){
+        if(err) console.log(err);
+        else console.log(info);
+    })
+    
+
     console.log(req.body);
     res.status(200).json('Hello, user');
 };
