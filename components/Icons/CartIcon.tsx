4d731f7574d99ceb765c577fd55c0f9487ea2f20
BisiDavi <oludavidconnect@gmail.com>
2021-05-13 16:11:36 +0100

    cart notification added

--- components/Icons/CartIcon.tsx
+++ components/Icons/CartIcon.tsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React from 'react';
+import { FaStarOfLife } from 'react-icons/fa';
+import styles from '../../styles/cart.module.css';
 
 const CartIcon = ({ count, cartClick }) => {
     return (
-        <div onClick={cartClick} className="cartIcon">
+        <div onClick={cartClick} className='cartIcon position-relative'>
+            {Number(count) > 0 && (
+                <div className='star'>
+                    <FaStarOfLife className={styles.cartSvg} />
+                </div>
+            )}
             <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                width="100%"
-                height="100%"
-                viewBox="0 0 164.9 196.4"
-                preserveAspectRatio="xMinYMax meet"
-                data-hook="svg-icon-9"
+                xmlns='http://www.w3.org/2000/svg'
+                version='1.1'
+                width='100%'
+                height='100%'
+                viewBox='0 0 164.9 196.4'
+                preserveAspectRatio='xMinYMax meet'
+                data-hook='svg-icon-9'
             >
                 <text
-                    x="84"
-                    y="131"
-                    dy=".35em"
-                    textAnchor="middle"
-                    className="cart-count"
-                    data-hook="items-count"
+                    x='84'
+                    y='131'
+                    dy='.35em'
+                    textAnchor='middle'
+                    className='cart-count'
+                    data-hook='items-count'
                 >
                     {count}
                 </text>
-                <path d="M81.9 11.5c-18.8 0-34.1 16-34.1 35.7v18.1h7.8V47.2c0-15.4 11.8-27.9 26.4-27.9 14.5 0 26.4 12.5 26.4 27.9v18.1h6.6V64h1.1V47.2c-.1-19.7-15.4-35.7-34.2-35.7z"></path>
-                <path d="M156.9 70.5v118H8v-118h148.9m8-8H0v134h164.9v-134z"></path>
+                <path d='M81.9 11.5c-18.8 0-34.1 16-34.1 35.7v18.1h7.8V47.2c0-15.4 11.8-27.9 26.4-27.9 14.5 0 26.4 12.5 26.4 27.9v18.1h6.6V64h1.1V47.2c-.1-19.7-15.4-35.7-34.2-35.7z'></path>
+                <path d='M156.9 70.5v118H8v-118h148.9m8-8H0v134h164.9v-134z'></path>
             </svg>
             <style jsx>
                 {`
@@ -35,6 +42,14 @@ const CartIcon = ({ count, cartClick }) => {
                         height: 100%;
                         width: 25px;
                         cursor: pointer;
+                    }
+                    .star {
+                        position: absolute;
+                        right: -5px;
+                        top: -5px;
+                    }
+                    .star svg {
+                        color: red;
                     }
                 `}
             </style>
