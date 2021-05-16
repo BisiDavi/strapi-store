5619b0e0e9b161df13dda292f4c676e2184f1813
BisiDavi <oludavidconnect@gmail.com>
2021-05-16 18:39:34 +0100

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {
     ProductsList,
     Newsletter,
     InstagramSlider,
+    Loading,
 } from '../components';
 import { HOMEPAGE_QUERY, HOMEPAGE_SEO_QUERY, request } from '../lib';
 import { HomeProps } from '../types';
@@ -50,6 +51,7 @@ const Home: NextPage<HomeProps> = ({
             <Pagelayout metaTags={seoData} title='Welcome'>
                 <div className='homepage position-relative'>
                     <HomepageSlider />
+                    {authCode === false && <Loading />}
                     <Collections />
                     <ProductsList products={allProducts} />
                     <Viewmore />
