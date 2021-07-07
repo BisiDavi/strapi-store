b15711d31c564c31fa36ce0613dc84745f844d57
BisiDavi <oludavidconnect@gmail.com>
2021-07-07 23:32:11 +0100

    main

--- pages/api/get-instagram-token/store-token.ts
+++ pages/api/get-instagram-token/store-token.ts
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../../../middlewares/database';
+import connectToDatabase from '../../../middlewares/database';
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
