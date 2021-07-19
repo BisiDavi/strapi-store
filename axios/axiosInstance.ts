5a170a340909cf6fc950c4ac4fb45cda21735a80
BisiDavi <oludavidconnect@gmail.com>
2021-07-20 00:33:47 +0100

    main

--- axios/axiosInstance.ts
+++ axios/axiosInstance.ts
@@ -22,7 +22,7 @@ export const axiosInstagramAPIInstance = axios.create({
 });
 
 export const axiosInstanceFlutterwave = axios.create({
-    baseURL: 'https://api.flutterwave.com/v3/',
+    baseURL: 'https://api.flutterwave.com/v3',
     headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_FLUTTERWAVE_SECRET_KEY}`,
         'content-type': 'application/json',
