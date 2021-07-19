5bb95d9cc9fe192183deedde03769e2e287f54c9
BisiDavi <oludavidconnect@gmail.com>
2021-07-19 15:08:33 +0100

    main

--- pages/api/checkout-notification.ts
+++ pages/api/checkout-notification.ts
@@ -10,19 +10,39 @@ export default async function CheckoutNotificationHandler(
 ) {
     const { method } = req;
     const { products, email, totalPrice, symbol } = req.body;
+
     const data = { products, email, totalPrice, symbol };
     const checkoutNotificationID =
         process.env.NEXT_PUBLIC_CHECKOUT_NOTIFICATION_ID;
+
     console.log('req.body', data);
+
+    const adminEmailAddress = [
+        process.env.NEXT_PUBLIC_ADMIN_EMAIL_ADDRESS,
+        process.env.NEXT_PUBLIC_DEVELOPER_EMAIL_ADDRESS,
+        'readydevfreelancer@gmail.com',
+    ];
+    //const email = isAdmin ? adminEmailAddress : userEmail;
+
+    const msg = {
+        to: adminEmailAddress,
+        from: {
+            email: process.env.NEXT_PUBLIC_SITE_EMAIL_ADDRESS,
+            name: "Jenjen's Luxury Wigs",
+        },
+        templateId: checkoutNotificationID,
+        dynamic_template_data: data,
+    };
 
     switch (method) {
         case 'POST':
             try {
-                sendEmail(sgMail, '', checkoutNotificationID, data, true)
-                    .then((response) => {
-                        console.log('response sendEmail', response);
-                    })
-                    .catch((error) => console.error('error sendEmail', error));
+                await sgMail.send(msg);
+                //sendEmail(sgMail, '', checkoutNotificationID, data, true)
+                //    .then((response) => {
+                //        console.log('response sendEmail', response);
+                //    })
+                //    .catch((error) => console.error('error sendEmail', error));
                 res.status(200).json({
                     success: true,
                     message: 'checkout email notification sent',
