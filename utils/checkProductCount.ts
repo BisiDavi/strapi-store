b960b782942b130e4129035f5e0c7f5ea6e6f6d1
BisiDavi <oludavidconnect@gmail.com>
2021-07-24 17:40:46 +0100

    main

--- utils/checkProductCount.ts
+++ utils/checkProductCount.ts
@@ -2,10 +2,10 @@ import { toast } from 'react-toastify';
 
 export default function checkProductCount(product) {
     let disableButton = false;
-    if (product.count >= product.productQuantity) {
+    if (product?.count >= product?.productQuantity) {
         disableButton = true;
         toast.error(
-            `We only have ${product.productQuantity} of ${product.title} in Stock`,
+            `We only have ${product?.productQuantity} of ${product?.title} in Stock`,
         );
     } else {
         disableButton = false;
