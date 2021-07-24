0798a29b1f6d65781964fe3fea1e634046e8efc4
BisiDavi <oludavidconnect@gmail.com>
2021-07-24 02:12:25 +0100

    main

--- components/Cart/CartTable.tsx
+++ components/Cart/CartTable.tsx
@@ -71,20 +71,6 @@ export default function CartTable({ products }) {
 
     const subtotalAmount = calculateSubtotal();
 
-    //let productArray = [];
-
-    //products.map(
-    //    (product) =>
-    //        (productArray = [
-    //            ...productArray,
-    //            {
-    //                amount: priceExchange(product.amount),
-    //                count: product.count,
-    //                image: product.image.responsiveImage.src,
-    //                title: product.title,
-    //            },
-    //        ]),
-    //);
     const productArray = formatProduct(products);
     console.log('productArray', productArray);
 
